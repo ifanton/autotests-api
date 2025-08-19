0953615d78776c3c1b41afc80b2b81d18d7814d1
Anton Panteleev <a.s.panteleev@yandex.ru>
2025-08-19 12:04:54 +0300

    Введение в HTTPX. Практическое задание: работа с HTTPX

---
+++ httpx_get_user_me.py
@@ -0,0 +1,25 @@
+import httpx
+import os
+
+
+os.environ["NO_PROXY"] = "*"  # отключение прокси
+
+login_payload = {
+  "email": "panteleev@example.com",
+  "password": "P@ssw0rd"
+}
+
+login_response = httpx.post("http://localhost:8000/api/v1/authentication/login", json=login_payload)
+login_response_data = login_response.json()
+
+accessToken = login_response_data['token']['accessToken']
+
+headers = {
+    "Authorization": f"Bearer {accessToken}"
+}
+
+get_me_response = httpx.get("http://localhost:8000/api/v1/users/me", headers=headers)
+get_me_response_data = get_me_response.json()
+
+print(get_me_response.status_code)
+print(get_me_response_data)
