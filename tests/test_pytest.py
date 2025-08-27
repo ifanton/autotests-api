5c1e596e7744e998a391b75eb5e255cf96dda6e1
Anton Panteleev <a.s.panteleev@yandex.ru>
2025-08-27 12:11:18 +0300

    Введение в Pytest

---
+++ tests/test_pytest.py
@@ -0,0 +1,34 @@
+import pytest
+
+
+def test_first_try():
+    greeting = "Hello"
+    assert greeting == "HELLO"
+
+def test_second_try():
+    print("World!")
+
+def test_assert_positive_case():
+    assert (1 + 1) == 2  # Ожидается, что тест пройдет
+
+def test_assert_negative_case():
+    assert (1 + 1) == 3, "Сумма 1 и 1 должна быть 2!"  # Тут должна быть ошибка
+
+def test_in_list():
+    assert 3 in [1, 2, 3, 4]
+
+def test_lists():
+    assert [1, 2, 3] == [1, 2, 4]
+
+def test_boolean():
+    is_authenticated = True
+    assert is_authenticated
+
+def test_zero_division():
+    with pytest.raises(ZeroDivisionError):
+        1 / 0
+
+
+class TestUserAuthentication:
+    def test_login(self):
+        assert 1 == 1
