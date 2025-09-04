f19c08aa2a1b271441214ef5d435d102678f0368
Anton Panteleev <a.s.panteleev@yandex.ru>
2025-09-04 19:51:25 +0300

    Настройки автотестов с Pydantic

--- clients/private_http_builder.py
+++ clients/private_http_builder.py
@@ -9,6 +9,8 @@ from pydantic import BaseModel, EmailStr
 
 from clients.event_hooks import curl_event_hook
 
+from config import settings
+
 
 class AuthenticationUserSchema(BaseModel, frozen=True):
     """
@@ -34,8 +36,8 @@ def get_private_http_client(user: AuthenticationUserSchema) -> Client:
     login_response = authentication_client.login(login_request)
 
     return Client(
-        timeout=100,
-        base_url="http://localhost:8000",
+        timeout=settings.http_client.timeout,
+        base_url=settings.http_client.client_url,
         headers={"Authorization": f"Bearer {login_response.token.access_token}"},
         event_hooks={"request": [curl_event_hook]}
     )
--- tools/assertions/files.py
+++ tools/assertions/files.py
@@ -8,6 +8,8 @@ from tools.assertions.base import assert_equal
 from tools.assertions.errors import assert_validation_error, assert_validation_error_response, \
     assert_internal_error_response
 
+from config import settings
+
 @allure.step("Check create file response")
 def assert_create_file_response(request: CreateFileRequestSchema, response: CreateFileResponseSchema):
     """
@@ -17,7 +19,7 @@ def assert_create_file_response(request: CreateFileRequestSchema, response: Crea
     :param response: Ответ API с данными файла
     :raises AssertionError: Если хотя бы одно поле не совпадает
     """
-    expected_url = f"http://localhost:8000/static/{request.directory}/{request.filename}"
+    expected_url = f"{settings.http_client.client_url}static/{request.directory}/{request.filename}"
 
     assert_equal(str(response.file.url), expected_url, "url")
     assert_equal(response.file.filename, request.filename, "filename")
