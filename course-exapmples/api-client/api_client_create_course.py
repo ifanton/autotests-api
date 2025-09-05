39f2a9f743d5122da681477c9de9edacfbfa8c50
Anton Panteleev <a.s.panteleev@yandex.ru>
2025-09-05 15:21:01 +0300

    Рефакторинг - все примеры собраны в отдельную папку, удалены файлы _pb2

--- course-exapmples/api-client/api_client_create_course.py
+++ course-exapmples/api-client/api_client_create_course.py
@@ -25,7 +25,7 @@ files_client = get_files_client(authentication_user)
 courses_client = get_courses_client(authentication_user)
 
 # Загружаем файл
-create_file_request = CreateFileRequestSchema(upload_file="./testdata/files/image.png")
+create_file_request = CreateFileRequestSchema(upload_file="../../testdata/files/image.png")
 create_file_response = files_client.create_file(create_file_request)
 print('Create file data:', create_file_response)
 
--- course-exapmples/api-client/api_client_create_exercise.py
+++ course-exapmples/api-client/api_client_create_exercise.py
@@ -27,7 +27,7 @@ courses_client = get_courses_client(authentication_user)
 exercises_client = get_exercises_client(authentication_user)
 
 # Загружаем файл
-create_file_request = CreateFileRequestSchema(upload_file="./testdata/files/image.png")
+create_file_request = CreateFileRequestSchema(upload_file="../../testdata/files/image.png")
 create_file_response = files_client.create_file(create_file_request)
 print('Create file data:', create_file_response)
 
