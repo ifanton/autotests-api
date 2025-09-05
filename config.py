efbe4603329428d045186992fbb88d0130d7ecd6
Anton Panteleev <a.s.panteleev@yandex.ru>
2025-09-05 14:27:42 +0300

    Добавляем информацию о переменных окружения в Allure отчет. Практика работы с переменным окружения в Allure

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
+import sys
+import platform
+
+from typing import Self
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import BaseModel, HttpUrl, FilePath
+from pydantic import BaseModel, HttpUrl, FilePath, DirectoryPath
 
 
 class HTTPClientConfig(BaseModel):
@@ -24,5 +29,24 @@ class Settings(BaseSettings):
 
     test_data: TestDataConfig
     http_client: HTTPClientConfig
+    allure_results_dir: DirectoryPath
+    os_info: str
+    python_version: str
 
-settings = Settings()
+
+    @classmethod
+    def initialize(cls) -> Self:
+        allure_results_dir = DirectoryPath("./allure-results")  # Создаем объект пути к папке
+        allure_results_dir.mkdir(exist_ok=True)  # Создаем папку allure-results, если она не существует
+
+        os_info = f'{platform.system()}, {platform.release()}'
+        python_version = sys.version
+
+        return Settings(
+            allure_results_dir=allure_results_dir,
+            os_info=os_info,
+            python_version=python_version
+        )
+
+
+settings = Settings.initialize()
